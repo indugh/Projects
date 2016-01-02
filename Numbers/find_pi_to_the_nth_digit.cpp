519f2d21a45cf26cedce7ebde70fc168b27d23fc
indugh <indugh@umich.edu>
2016-01-01 21:06:41 -0500

    Let's start this!

---
+++ Numbers/find_pi_to_the_nth_digit.cpp
@@ -0,0 +1,12 @@
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+
+
+int main() {
+	int num_digits = 0;
+	cin >> num_digits;
+	cout << std::set_precision(num_digits);
+	
+}
